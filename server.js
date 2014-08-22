1a7e43df23ee13ad5d45e9ca1b82afd0d2afa6a1
Manish Bansal <qualityking@gmail.com>
2014-08-22 17:41:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,28 +2,12 @@
 
 var http = require("http");
 var inline = require("./inline");
-var url = require("url");
-
 inline.compile("www");		
 
 http.createServer(function (request, response){
-  navigate(request, response);
-  response.writeHead(200, {"Content-Type": "text/html"});  
-  response.end();
+  response.writeHead(200, {"Content-Type": "text/html"});  		
+  inline.navigate(request, response);
 
 }).listen(8080);
 console.log('Server running at http://127.0.0.1:8080/');
-
-
-function navigate(request, response){
-
- var pathname = url.parse(request.url).pathname;
- console.log(pathname);
- pathname = "compiled/www_index.js";
- var file = require(pathname);
- //evel();
- file.www_index(request, response);
-}
-
-
 
