dd87e9007740920f9fd421f4bef86b651e55cfe0
Manish Bansal <qualityking@gmail.com>
2014-08-22 17:40:13 +0530

    Update inline.js

--- inline.js
+++ inline.js
@@ -3,18 +3,48 @@ var path = require('path');
 var readline = require('readline');
 var stream = require('stream');
 var webroot = require('./webroot');
+var url = require("url");
+var rootdir = ""; 
+
+exports.rootdir = rootdir; 
+
+exports.navigate = function navigate(request, response){
+ var _rootdir = this.rootdir; 
+ var pathname = url.parse(request.url).pathname; 
+ var jsfilename = "";	
+	
+	if(pathname.toLowerCase()=="/favicon.ico"){
+		return; 
+	}
+	else if(pathname=="/" || pathname.toLowerCase()=="index.htm" || pathname.toLowerCase()=="index.html") {
+		jsfilename = "./compiled/"+ _rootdir +"_index.js";
+	}else{
+		var _newfilename = './compiled/'+ _rootdir + pathname.replace(/\//g, "_");
+		var j = _newfilename.lastIndexOf('.');
+		var extn = _newfilename.substring(j).toLowerCase()
+		jsfilename = _newfilename.replace(extn,".js");	
+	}
+	if (path.existsSync(jsfilename)) { 
+		var file = require(jsfilename);
+		file.loadPage(request, response);
+		response.end("");
+	}else {
+		console.log("404 file not found : ", pathname);
+		response.end("");
+	}
+	 
+}
 
 
-
-var NodeScript = ""; 
-
-function compile(root){
+exports.compile = function compile(root){
+	this.rootdir = root;
 	var filelist = webroot.getFileList(root)
 	filelist.forEach(doCompile);
 }
 
 function doCompile(fileName) {
- 	
+ 	console.log("compiling file : ", fileName);
+	
 	var _newfilename = 'compiled/' + fileName.replace('/','_');
 	var j = _newfilename.lastIndexOf('.');
 	var extn = _newfilename.substring(j).toLowerCase()
@@ -23,11 +53,10 @@ function doCompile(fileName) {
 	if (path.existsSync(newfilename)) { 
 		fs.unlink(newfilename);
 	}
-	fs.appendFile(newfilename, "exports.www_index = function www_index(request,response) {");
+	fs.appendFile(fileName, '\n');
+	fs.appendFile(newfilename, "exports.loadPage = function (request,response) {");
 	
-	//setTimeout(function() {
-	//}, 10000);
-	
+
 	var scripletStart= false; 
 	var scripletInlineStart= false; 
 	
@@ -65,8 +94,10 @@ function doCompile(fileName) {
 			}
 		}
 		
-		if(!scripletStart && line!="" && !scripletInlineStart){
-			line =  "response.write('"  + line + "'); " + "\n";  
+		if(!scripletStart && !scripletInlineStart){
+			if(!line==""){
+				line =  "response.write('"  + line + "'); " + "\n";  
+			}		
 		}
 		else {
 			line =  line + "\n"; 
@@ -81,17 +112,6 @@ function doCompile(fileName) {
 		fs.appendFile(newfilename, '}');
 	});
 
-}
-
-
-exports.compile = compile;
-
-
-
-function generateNavigate(filePath){
-
-	var module = require(filePath); 
-	
 }
 
 
